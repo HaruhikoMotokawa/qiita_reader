8924be3149d0c81d2e22a84e62d0621d491b1891
本川　晴彦 <h.motokawa@salop.co.jp>
2024-07-15 11:04:37 +0900

    chore: ファイル移動

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:qiita_reader/core/app.dart';
+import 'package:qiita_reader/presentations/app.dart';
 import 'package:qiita_reader/presentations/app_start_up/app_start_up_screen.dart';
 
 void main() {
